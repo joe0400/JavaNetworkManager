6c4a0a23054f7f315091025650583341ad39ffd6
Joseph Scannell <joe0400@gmail.com>
2025-11-14 22:23:17 +0000

    updating the package name and starting to stub out the files needed

---
+++ lib/src/main/java/org/NetworkManager/LoggingDomain.java
@@ -0,0 +1,8 @@
+package org.NetworkManager;
+
+import java.util.logging.Level;
+
+public class LoggingDomain{
+	public final Level level;
+	public final String domain;
+}
---
+++ lib/src/main/java/org/NetworkManager/NetworkManager.java
@@ -0,0 +1,67 @@
+package org.NetworkManager;
+
+import java.util.logging.Level;
+
+public class NetworkManager{
+	
+	public static final long PLATFORM = 1;
+	public static final long RFKILL = 1 << 1;
+	public static final long ETHER = 1 << 2;
+	public static final long WIFI = 1 << 3;
+	public static final long BT = 1 << 4;
+	public static final long MB = 1 << 5;
+	public static final long DHCP4 = 1 << 6;
+	public static final long DHCP6 = 1 << 7;
+	public static final long PPP = 1 << 8;
+	public static final long WIFI_SCAN = 1 << 9;
+       	public static final long IP4 = 1 << 10;
+	public static final long IP6 = 1 << 11;
+	public static final long AUTOIP4 = 1 << 12;
+	public static final long DNS = 1 << 13;
+	public static final long VPN = 1 << 14;
+	public static final long SHARING = 1 << 15;
+	public static final long SUPPLICANT = 1 << 16;
+	public static final long AGENTS = 1 << 17;
+	public static final long SETTINGS = 1 << 18;
+	public static final long SUSPEND = 1 << 19;
+	public static final long CORE = 1 << 20;
+	public static final long DEVICE = 1 << 21;
+	public static final long OLPC = 1 << 22;
+	public static final long WIMAX = 1 << 23;
+	public static final long INFINIBAND = 1 << 24;
+	public static final long FIREWALL = 1 << 25;
+	public static final long ADSL = 1 << 26;
+	public static final long BOND = 1 << 27;
+	public static final long VLAN = 1 << 28;
+	public static final long BRIDGE = 1 << 29;
+	public static final long DBUS_PROPS = 1 << 30;
+	public static final long TEAM = 1 << 31;
+	public static final long CONCHECK = 1 << 32;
+	public static final long DCB = 1 << 33;
+	public static final long DISPATCH = 1 << 34;
+	public static final long AUDIT = 1 << 35;	
+	
+	public static final long NONE = 0;
+	public static final long ALL = 
+		PLATFORM | RFILL | ETHER | WIFI | BT | MB | DHCP4 |
+		DHCP6 | PPP | WIFI_SCAN | IP4 | IP6 | AUTOIP4 | 
+		DNS | VPN | SHARING | SUPPLICANT | AGENTS |
+		SETTING | SUSPEND | CORE | DEVICE | OLPC | 
+		WIMAX | INFINIBAND | FIREWALL |ADSL_BOND |
+		TEAM | CONCHECK | DCB | DISPATCH | AUDIT;
+
+	//TODO: Find out what does DHCP and IP actually log at in Network Manager
+
+	/**
+	 * Sets the level of the logger for the NetworkManager from D-Bus
+	 * @param level the level of the logger
+	 * @param domain the domain of what to log, a bitmask or just NONE to disable logging
+	 * */
+	public native void setLogging(Level level, long domain);
+	
+	/**
+	 * 
+	 * */
+	public native LoggingDomain getLogging(); 
+	
+}
